dcff175f48a8637e96add4c1673e3c649d06eda3
salhmanvir <Manvir.Salh@kpmg.co.uk>
2023-09-04 14:27:28 +0100

    MS-7 adding numbers and dates translation file

--- app/src/TextFiles/translations.ts
+++ app/src/TextFiles/translations.ts
@@ -1,8 +1,10 @@
 import { spotifyEmbedLinks } from "./spotifyEmbedLinks";
 import { music } from "./music";
+import { numbersAndDates } from "./numbersAndDates";
 
 
 export const enResources = {
   spotifyEmbedLinks: spotifyEmbedLinks,
-  music: music
+  music: music,
+  numbersAndDates: numbersAndDates
 };
