a24d77bf977320d292542e4b211f039873731a70
salhmanvir <Manvir.Salh@kpmg.co.uk>
2023-09-06 20:00:56 +0100

    MS-7 Adding rock songs list

--- app/src/Pages/Music/index.tsx
+++ app/src/Pages/Music/index.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import { Box, Typography } from "@mui/material";
 import { makeStyles } from "@mui/styles";
 import createTheme from "@mui/material/styles/createTheme";
-import { TopDnb2023 } from "../../Constants/TopSongsLists";
+import { TopDnb2023, TopRockMetal2023 } from "../../Constants/TopSongsLists";
 import { TopSongsList } from "./TopSongsList";
 import { useTranslation } from "react-i18next";
 
 const Music: React.FC = () => {
   const styles = useStyles();
   const topDnb2023 = TopDnb2023();
+  const topRockMetal2023 = TopRockMetal2023();
   const { t } = useTranslation("music");
   const{t:tyear} = useTranslation("numbersAndDates")
 
@@ -22,7 +23,7 @@ const Music: React.FC = () => {
       <TopSongsList songUrlList={topDnb2023} title={t("dnb")} />
     </Box>
     <Box className={styles.listContainer}>
-      <TopSongsList songUrlList={topDnb2023} title={t("rockMetal")} />
+      <TopSongsList songUrlList={topRockMetal2023} title={t("rockMetal")} />
     </Box>
     </Box>
     </Box>
@@ -36,12 +37,10 @@ const useStyles = makeStyles({
     marginLeft: '20%',
   },
   title: {
-    // padding: theme.spacing(5),
     justifyContent: "center",
     display: "flex",
   },
   mainContainer: {
-    // padding: theme.spacing(6),
     alignItems: "center",
     display: "flex",
     flexDirection: "row",
