909b33dc5f8d7c9e11c167497a0b36b2742171bd
salhmanvir <Manvir.Salh@kpmg.co.uk>
2023-09-14 10:48:05 +0100

    MS-10 textfield theme

--- app/src/Pages/Home/index.tsx
+++ app/src/Pages/Home/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { Box, TextField, Typography } from "@mui/material";
+import { Box, TextField, ThemeProvider, Typography } from "@mui/material";
 import { makeStyles } from "@mui/styles";
 import { useUserName } from "../../Store/Username/hooks";
 import { useTranslation } from "react-i18next";
 import { PrimaryButton } from "../../Components/PrimaryButton";
+import { textFieldTheme } from "../../Theme/TextFieldTheme";
 
 const Home: React.FC = () => {
   const styles = useStyles();
@@ -20,31 +21,29 @@ const Home: React.FC = () => {
   return (
     <Box className={styles.container}>
       <Box className={styles.column}>
-        <Typography variant="h1">{t('welcome')}</Typography>
-        <Box marginTop={'5%'}>
-        <Typography variant="body1">{t('welcomeMessage')}</Typography>
+        <Typography variant="h1">{t("welcome")}</Typography>
+        <Box marginTop={"5%"}>
+          <Typography variant="body1">{t("welcomeMessage")}</Typography>
         </Box>
         <Box className={styles.username}>
-          <TextField
-            variant="outlined"
-            value={userNameEntered}
-            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-              setUserNameEntered(event.target.value);
-              
-            }}
-          />
-          <Box marginLeft={'10%'} marginTop={'3%'}>
-          <PrimaryButton
-          onClick={onClickUpdateUserName}
-          text={t('update')}
-          />
+          <ThemeProvider theme={textFieldTheme}>
+            <TextField
+              variant="outlined"
+              autoComplete="off"
+              value={userNameEntered}
+              onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                setUserNameEntered(event.target.value);
+              }}
+            />
+          </ThemeProvider>
+          <Box marginLeft={"10%"} marginTop={"3%"}>
+            <PrimaryButton onClick={onClickUpdateUserName} text={t("update")} />
           </Box>
         </Box>
       </Box>
     </Box>
   );
 };
-
 
 const useStyles = makeStyles({
   container: {
