8b59d188aaac48633319a08891dd6879dff74f85
salhmanvir <Manvir.Salh@kpmg.co.uk>
2023-10-22 10:58:12 +0100

    MS18 Muisc page changes

--- app/src/Pages/Music/index.tsx
+++ app/src/Pages/Music/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Tooltip, Typography } from "@mui/material";
 import { makeStyles } from "@mui/styles";
 import createTheme from "@mui/material/styles/createTheme";
 import { TopDnb2023, TopRockMetal2023 } from "../../Constants/topSongsLists";
@@ -14,35 +14,60 @@ const Music: React.FC = () => {
   const topDnb2023 = TopDnb2023();
   const topRockMetal2023 = TopRockMetal2023();
   const { t } = useTranslation("music");
-  const{t:tyear} = useTranslation("numbersAndDates")
+  const { t: tyear } = useTranslation("numbersAndDates");
+  const [haveAccessToken, setHaveAccessToken] = React.useState<boolean>(false);
 
   //only works in prod - comment out and use button below for dev
   // React.useEffect(() => {
   //   fetchTokenRequest()
   // },[])
 
+  React.useEffect(() => {
+    const checkAccessToken = localStorage.getItem("access_token")
+      ? true
+      : false;
+    setHaveAccessToken(checkAccessToken);
+  }, []);
+
   return (
     <>
-     <Box className={styles.links}>
-        <LinkButton 
-        text={t('spotifyApi')}
-        route={routes.personalisedSpotify}
-        />
+      <Box className={styles.links}>
+        {!haveAccessToken && (
+          <Tooltip title='Authorise Spotify access in the "My Profile" area'>
+            <span>
+              <LinkButton
+                text={t("yourSpotify")}
+                route={routes.personalisedSpotify}
+                disabled={!haveAccessToken}
+              />
+            </span>
+          </Tooltip>
+        )}
+        {haveAccessToken && (
+          <LinkButton
+            text={t("yourSpotify")}
+            route={routes.personalisedSpotify}
+            disabled={!haveAccessToken}
+          />
+        )}
       </Box>
       {/* <Button onClick = {() => {fetchTokenRequest()}}></Button> */}
-    <Box className={styles.pageParameters}>
-      <Box className={styles.title}>
-    <Typography variant="h1">{tyear('2023')}</Typography>
-    </Box>
-    <Box className={styles.mainContainer}>
-    <Box className={styles.listContainer}>
-      <TopSongsList songUrlList={topDnb2023} title={t("dnb")} />
-    </Box>
-    <Box className={styles.listContainer}>
-      <TopSongsList songUrlList={topRockMetal2023} title={t("rockMetal")} />
-    </Box>
-    </Box>
-    </Box>
+      <Box className={styles.pageParameters}>
+        <Box className={styles.title}>
+          <Typography variant="h1">{tyear("2023")}</Typography>
+        </Box>
+        <Box className={styles.mainContainer}>
+          <Box className={styles.listContainer}>
+            <TopSongsList songUrlList={topDnb2023} title={t("dnb")} />
+          </Box>
+          <Box className={styles.listContainer}>
+            <TopSongsList
+              songUrlList={topRockMetal2023}
+              title={t("rockMetal")}
+            />
+          </Box>
+        </Box>
+      </Box>
     </>
   );
 };
@@ -50,8 +75,8 @@ const Music: React.FC = () => {
 const theme = createTheme();
 const useStyles = makeStyles({
   pageParameters: {
-    width: '60%', 
-    marginLeft: '20%',
+    width: "60%",
+    marginLeft: "20%",
   },
   title: {
     justifyContent: "center",
