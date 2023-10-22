030d2929316c3fd08f51aa2a9886de2761afa2c2
salhmanvir <Manvir.Salh@kpmg.co.uk>
2023-10-22 10:52:04 +0100

    MS18 My profile routes

--- app/src/Constants/Routes.ts
+++ app/src/Constants/Routes.ts
@@ -2,7 +2,8 @@ export const routes = {
     music: "/music",
     dnb: "/music/dnb",
     home: "/",
-   personalisedSpotify: "/music/personalisedSpotify",
+    personalisedSpotify: "/music/personalisedSpotify",
+    myProfile: '/myProfile'
 
 
 }
