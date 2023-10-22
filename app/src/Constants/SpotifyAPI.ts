ceb39848b7068a37ef459d58443e71680128d2e8
salhmanvir <Manvir.Salh@kpmg.co.uk>
2023-10-22 10:54:10 +0100

    MS18 My profile page

--- app/src/Constants/SpotifyAPI.ts
+++ app/src/Constants/SpotifyAPI.ts
@@ -1,6 +1,6 @@
 export const spotifyAPI = {
     clientId: '44deba64e2b04230a4e7c818ca419918',
-    redirectUri: 'http://localhost:3000/music',
+    redirectUri: 'http://localhost:3000/myProfile',
     scope: 'user-read-private user-read-email user-top-read user-read-recently-played playlist-read-private',
     authoriseUrl: 'https://accounts.spotify.com/authorize?',
     
---
+++ app/src/Pages/MyProfile/index.tsx
@@ -0,0 +1,61 @@
+import { Box, Typography } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import React from "react";
+import { useTranslation } from "react-i18next";
+import { PrimaryButton } from "../../Components/PrimaryButton";
+import { AuthorisationRequest } from "../../Store/SpotifyAPI/authorisationRequest";
+import { fetchTokenRequest } from "../../Store/SpotifyAPI/fetchTokenRequest";
+
+export const MyProfile: React.FC = () => {
+  const styles = useStyles();
+  const { t } = useTranslation("myProfile");
+  const [haveAccessToken, setHaveAccessToken] = React.useState<boolean>(false);
+
+  const fetchAuthorisation = React.useCallback(() => {
+    AuthorisationRequest();
+  }, []);
+
+  React.useEffect(() => {
+    fetchTokenRequest().then(() => {
+      const checkAccessToken = localStorage.getItem("access_token")
+        ? true
+        : false;
+      setHaveAccessToken(checkAccessToken);
+    });
+  }, []);
+
+  return (
+    <Box className={styles.listContainer}>
+        <Box marginBottom={4}>
+        <Typography variant="h1">{t('myProfile')}</Typography>
+        </Box>
+      <Typography variant="h3">{t('spotifyAuthorisation')}</Typography>
+      <Box className={styles.container} marginTop={2}>
+        <Typography variant="body1">
+            {haveAccessToken ?  t('haveAuthorised') :  t('spotifyDescription')}
+        </Typography>
+        <Box marginLeft={2}>
+        <PrimaryButton
+          text={t("authorise")}
+          onClick={fetchAuthorisation}
+          disabled={haveAccessToken}
+        />
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+const useStyles = makeStyles({
+  container: {
+    alignItems: "flex-start",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  listContainer: {
+    alignItems: "flex-start",
+    display: "flex",
+    flexDirection: "column",
+  },
+});
