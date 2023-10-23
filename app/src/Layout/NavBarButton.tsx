d94b5be3d4fe3641406de56edcb5218d1b5678be
salhmanvir <Manvir.Salh@kpmg.co.uk>
2023-10-23 09:19:17 +0100

    MS21 My profile button

--- app/src/Layout/NavBarButton.tsx
+++ app/src/Layout/NavBarButton.tsx
@@ -1,29 +1,58 @@
 import { Button } from "@mui/material";
-import React from "react";
-
+import React, { ReactNode } from "react";
+import { makeStyles } from "@mui/styles";
 interface NavBarButtonProps {
   onClick: () => void;
   text: string;
+  icon?: ReactNode;
 }
 
-const NavBarButton: React.FC<NavBarButtonProps> = ({ onClick, text }) => {
+const NavBarButton: React.FC<NavBarButtonProps> = ({ onClick, text, icon }) => {
+  const styles = useStyles();
   return (
-    <Button
-      onClick={onClick}
-      sx={{
-        my: 2,
-        color: "white",
-        display: "block",
-        bgcolor: "#858585",
-        ":hover": {
-          bgcolor: "#656565",
-          color: "white",
-        },
-      }}
-    >
-      {text}
-    </Button>
+    <>
+      {icon ? (
+        <Button
+          className={styles.button}
+          onClick={onClick}
+          sx={{
+            my: 2,
+            color: "white",
+            bgcolor: "#858585",
+            ":hover": {
+              bgcolor: "#656565",
+              color: "white",
+            },
+          }}
+        >
+          {text} &nbsp; {icon}
+        </Button>
+      ) : (
+        <Button
+          className={styles.button}
+          onClick={onClick}
+          sx={{
+            my: 2,
+            color: "white",
+            bgcolor: "#858585",
+            ":hover": {
+              bgcolor: "#656565",
+              color: "white",
+            },
+          }}
+        >
+          {text}
+        </Button>
+      )}
+    </>
   );
 };
+
+const useStyles = makeStyles({
+  button: {
+    display: "flex",
+    alignItems: "center",
+  },
+});
 
 export { NavBarButton };
