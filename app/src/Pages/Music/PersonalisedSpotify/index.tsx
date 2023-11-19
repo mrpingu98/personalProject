bb88d4289b5a8f5db90669147b1a5f07be84d233
salhmanvir <Manvir.Salh@kpmg.co.uk>
2023-11-19 13:58:47 +0000

    MS24 cleaning up redundant api calls

--- app/src/Pages/Music/PersonalisedSpotify/index.tsx
+++ app/src/Pages/Music/PersonalisedSpotify/index.tsx
@@ -2,8 +2,6 @@
 import React from "react";
 import { Box, Typography } from "@mui/material";
 import { makeStyles } from "@mui/styles";
-import { getSpotifyUserProfile } from "../../../Store/SpotifyAPI/getSpotifyUserProfile";
-import { refreshAccessToken } from "../../../Store/SpotifyAPI/refreshAccessToken";
 import { useTranslation } from "react-i18next";
 import {
   withRefreshAccessToken,
@@ -13,8 +11,6 @@ import {
   withSpotifyUserPlaylists,
 } from "../../../Store/SpotifyAPI/components";
 import { compose } from "redux";
-import { fetchPersonalisedSpotifyData } from "../../../Store/SpotifyAPI/fetchPersonalisedSpotifyData";
-import { PrimaryButton } from "../../../Components/PrimaryButton";
 import { useSpotifyData } from "../../../Store/SpotifyAPI/hooks";
 
 const PersonalisedSpotify = compose<React.FC>(
@@ -27,23 +23,7 @@ const PersonalisedSpotify = compose<React.FC>(
   const styles = useStyles();
   const { t } = useTranslation("personalisedSpotify");
 
-  const getUserProfile = React.useCallback(() => {
-    getSpotifyUserProfile();
-  }, []);
-
-  const refreshToken = React.useCallback(() => {
-    refreshAccessToken();
-  }, []);
-
-  const { spotifyUserData, userTopArtists, userTopTracks, userPlaylists } =
-    useSpotifyData();
-
-  // only works in prod - comment out and use buttons below for dev
-  // React.useEffect (() => {
-  //   if (hasUserAuthorised){
-  //     fetchPersonalisedSpotifyData()
-  //   }
-  // },[hasUserAuthorised])
+  const { spotifyUserData, userTopArtists, userTopTracks, userPlaylists } = useSpotifyData();
 
   return (
     <Box className={styles.mainContainer}>
