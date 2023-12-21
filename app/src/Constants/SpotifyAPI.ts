5fb6d2541a14f91350ea5dd4735ad927245c50b4
salhmanvir <Manvir.Salh@kpmg.co.uk>
2023-12-21 16:06:00 +0000

    MS36 undoing local settings

--- app/src/Constants/SpotifyAPI.ts
+++ app/src/Constants/SpotifyAPI.ts
@@ -1,8 +1,7 @@
 export const spotifyAPI = {
     clientId: '44deba64e2b04230a4e7c818ca419918',
-    redirectUri: 'http://localhost:3000/myProfile',
-    // 'https://green-bay-02bd95803.4.azurestaticapps.net/myProfile',
+    redirectUri: 'https://green-bay-02bd95803.4.azurestaticapps.net/myProfile',
+    //'http://localhost:3000/myProfile'
     scope: 'user-read-private user-read-email user-top-read user-read-recently-played playlist-read-private',
     authoriseUrl: 'https://accounts.spotify.com/authorize?',
-    
 };
