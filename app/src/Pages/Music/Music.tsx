7f4522af05488e62589c4d068041c5985ae3bfcf
mrpingu98 <salh98manvir@gmail.com>
2024-03-07 16:00:19 +0000

    MS47 Updating mUSIC PAGE STYLING

--- app/src/Pages/Music/Music.tsx
+++ app/src/Pages/Music/Music.tsx
@@ -1,18 +1,16 @@
 import React from "react";
 import { Box, Typography } from "@mui/material";
-import { makeStyles } from "@mui/styles";
-import createTheme from "@mui/material/styles/createTheme";
 import { TopSongsList } from "./TopSongsList";
 import { useTranslation } from "react-i18next";
 import { LinkButton } from "../../Components/LinkButton";
 import { routes } from "../../Constants/Routes/RoutesEndpoints";
 import useTopSongsList from "../../Hooks/useTopSongsList";
+import { styled } from '@mui/system';
 
 const Music: React.FC = () => {
-  const styles = useStyles();
   const { t } = useTranslation("music");
   const { t: tyear } = useTranslation("numbersAndDates");
-  const {dnbList2023, rockList2023} = useTopSongsList()
+  const { dnbList2023, rockList2023 } = useTopSongsList()
   const [haveAccessToken, setHaveAccessToken] = React.useState<boolean>(false);
 
   React.useEffect(() => {
@@ -23,15 +21,17 @@ const Music: React.FC = () => {
   return (
     <>
       {!haveAccessToken &&
-        <Box marginTop={"5%"} className={styles.title}>
-          <Typography variant="body2" data-test-id={"unauthorised-spotify-description"}>
-            <em>
-              {t("authoriseSpotify")}
-            </em>
-          </Typography>
-        </Box>
+        <Title>
+          <Box marginTop={"5%"}>
+            <Typography variant="body2" data-test-id={"unauthorised-spotify-description"}>
+              <em>
+                {t("authoriseSpotify")}
+              </em>
+            </Typography>
+          </Box>
+        </Title>
       }
-      <Box className={styles.links}>
+      <Links>
         <LinkButton
           text={t("yourSpotify")}
           route={routes.personalisedSpotify}
@@ -44,59 +44,59 @@ const Music: React.FC = () => {
             route={routes.mixes}
           />
         </Box>
-      </Box>
-      <Box className={styles.pageParameters}>
-        <Box className={styles.title}>
+      </Links>
+      <PageParameters>
+        <Title>
           <Typography variant="h1" data-test-id={"music-title"}>{`${t('manvir')} ${tyear("2023")}`}</Typography>
-        </Box>
-        <Box className={styles.mainContainer}>
-          <Box className={styles.listContainer} data-test-id={'dnb-title'}>
+        </Title>
+        <MainContainer>
+          <ListContainer data-test-id={'dnb-title'}>
             <TopSongsList songUrlList={dnbList2023} title={t("dnb")} />
-          </Box>
-          <Box className={styles.listContainer} data-test-id={"rock-title"}>
+          </ListContainer>
+          <ListContainer data-test-id={"rock-title"}>
             <TopSongsList
               songUrlList={rockList2023}
               title={t("rockMetal")}
             />
-          </Box>
-        </Box>
-      </Box>
+          </ListContainer>
+        </MainContainer>
+      </PageParameters>
     </>
   );
 };
 
-const theme = createTheme();
-const useStyles = makeStyles({
-  pageParameters: {
-    width: "60%",
-    marginLeft: "20%",
-  },
-  title: {
-    justifyContent: "center",
-    display: "flex",
-  },
-  mainContainer: {
-    alignItems: "center",
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    '@media (max-width: 600px)': {
-      flexDirection: 'column', // Apply for screens up to 600px (xs screens)
-    },
-  },
-  listContainer: {
-    padding: theme.spacing(4),
-    alignItems: "center",
-    display: "flex",
-    flexDirection: "column",
-  },
-  links: {
-    alignItems: "center",
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: 'center',
-    marginTop: theme.spacing(4)
+const PageParameters = styled('div')({
+  width: "60%",
+  marginLeft: "20%",
+});
+const Title = styled('div')({
+  justifyContent: "center",
+  display: "flex",
+});
+
+const MainContainer = styled('div')({
+  alignItems: "center",
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "space-between",
+  '@media (max-width: 600px)': {
+    flexDirection: 'column', // Apply for screens up to 600px (xs screens)
   },
 });
+
+const ListContainer = styled('div')(({ theme }) => ({
+  padding: theme.spacing(4),
+  alignItems: "center",
+  display: "flex",
+  flexDirection: "column",
+}));
+
+const Links = styled('div')(({ theme }) => ({
+  alignItems: "center",
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: 'center',
+  marginTop: theme.spacing(4)
+}));
 
 export { Music };
