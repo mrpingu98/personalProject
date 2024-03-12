cf9f797dbba4da1d8bf20939e2b2bb99d256ad1a
mrpingu98 <salh98manvir@gmail.com>
2024-03-12 14:54:00 +0000

    MS48 Product payload types

--- app/src/Constants/Types/Product.ts
+++ app/src/Constants/Types/Product.ts
@@ -1,6 +1,18 @@
-export type Product = {
+export type AddProductPayload = {
     name: string,
     description: string,
     price: number,
     imageUrl: string
+}
+
+export type EditProductPayload = {
+    name: string,
+    newName?: string,
+    description?: string,
+    price?: number,
+    imageUrl?: string
+}
+
+export type DeleteProductPayload = {
+    name: string
 }
