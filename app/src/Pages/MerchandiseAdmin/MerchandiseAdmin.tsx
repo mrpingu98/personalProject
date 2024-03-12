f9e14fca543a73b3be03f31c4f2e160565df3c24
mrpingu98 <salh98manvir@gmail.com>
2024-03-12 14:54:31 +0000

    MS48 Admin cleanup

--- app/src/Pages/MerchandiseAdmin/MerchandiseAdmin.tsx
+++ app/src/Pages/MerchandiseAdmin/MerchandiseAdmin.tsx
@@ -1,48 +1,96 @@
 import { Button, TextField, Typography } from "@mui/material"
-import { useQueryPost } from "../../Hooks/useQueryPost";
-import { Product } from "../../Constants/Types/Product";
+import { useQueryDelete, useQueryPost, useQueryPut } from "../../Hooks/useMutations";
+import { AddProductPayload, DeleteProductPayload, EditProductPayload } from "../../Constants/Types/Product";
 import React from "react";
 
 const MerchandiseAdmin: React.FC = () => {
-    const [name, setName] = React.useState<string>('');
-    const [description, setDescription] = React.useState<string>('');
-    const [price, setPrice] = React.useState<string>('');
-    const [imageUrl, setImageUrl] = React.useState<string>('');
+    const [deleteProductPayload, setDeleteProductPayload] = React.useState<DeleteProductPayload>({name: ''});
+    const [addProductPayload, setAddProductPayload] = React.useState<AddProductPayload>({
+      name: '',
+      description: '',
+      price: parseFloat('0'),
+      imageUrl: ''})
+    
+    const [editProductPayload, setEditProductPayload] = React.useState<EditProductPayload>({
+      name: '',
+      newName:'',
+      description: '',
+      price: parseFloat('0'),
+      imageUrl: ''
+    })
 
-    const addProduct: Product = {
-        name: name,
-        description: description,
-        price: parseFloat(price),
-        imageUrl: imageUrl
-    }
+    const {mutation: mutationAddProduct} = useQueryPost('https://localhost:7119/product', addProductPayload);
 
+    const {mutation: mutationEditProduct} = useQueryPut('https://localhost:7119/product', editProductPayload)
 
-    const { mutation } = useQueryPost('https://localhost:7119/product', addProduct);
+    const {mutation: mutationDeleteProduct} = useQueryDelete('https://localhost:7119/product', deleteProductPayload)
 
     return (
         <>
         <Typography variant='h1'>Add Product</Typography>
         <div>Name</div>
-        <TextField onChange={(e) => setName(e.target.value)}/>
+        <TextField onChange={(e) => setAddProductPayload({...addProductPayload, name: e.target.value})}/>
         <div>Description</div>
-        <TextField onChange={(e) => setDescription(e.target.value)}/>
+        <TextField onChange={(e) => setAddProductPayload({...addProductPayload, description: e.target.value})}/>
         <div>Price</div>
-        <TextField onChange={(e) => setPrice(e.target.value)}/>
+        <TextField onChange={(e) => setAddProductPayload({...addProductPayload, price: parseFloat(e.target.value)})}/>
         <div>ImageUrl</div>
-        <TextField onChange={(e) => setImageUrl(e.target.value)}/>
-        <Button onClick={() => mutation.mutate()}>Add Product</Button>
+        <TextField onChange={(e) => setAddProductPayload({...addProductPayload, imageUrl: e.target.value})}/>
+        <Button onClick={() => mutationAddProduct.mutate()}>Add Product</Button>
         <div>
-        {mutation.isPending ? (
+        {mutationAddProduct.isPending ? (
           'Adding todo...'
         ) : (
           <>
-            {mutation.isError ? (
-              <div>An error occurred: {mutation.error.message}</div>
+            {mutationAddProduct.isError ? (
+              <div>An error occurred: {mutationAddProduct.error.message}</div>
             ) : null}
 
-            {mutation.isSuccess ? <div>Todo added!</div> : null}
+            {mutationAddProduct.isSuccess ? <div>Todo added!</div> : null}
+          </>
+        )}
+      </div>
 
-       
+      <Typography variant='h1' marginTop='5%'>Edit Product</Typography>
+        <div>Name</div>
+        <TextField onChange={(e) => setEditProductPayload({...editProductPayload, name: e.target.value})}/>
+        <div>New Name</div>
+        <TextField onChange={(e) => setEditProductPayload({...editProductPayload, newName: e.target.value})}/>
+        <div>Description</div>
+        <TextField onChange={(e) => setEditProductPayload({...editProductPayload, description: e.target.value})}/>
+        <div>Price</div>
+        <TextField onChange={(e) => setEditProductPayload({...editProductPayload, price: parseFloat(e.target.value)})}/>
+        <div>ImageUrl</div>
+        <TextField onChange={(e) => setEditProductPayload({...editProductPayload, imageUrl: e.target.value})}/>
+        <Button onClick={() => mutationEditProduct.mutate()}>Edit Product</Button>
+        <div>
+        {mutationEditProduct.isPending ? (
+          'Adding todo...'
+        ) : (
+          <>
+            {mutationEditProduct.isError ? (
+              <div>An error occurred: {mutationEditProduct.error.message}</div>
+            ) : null}
+
+            {mutationEditProduct.isSuccess ? <div>Todo added!</div> : null}
+          </>
+        )}
+      </div>
+
+      <Typography variant='h1' marginTop='5%'>Delete Product</Typography>
+        <div>Name</div>
+        <TextField onChange={(e) => setDeleteProductPayload({... deleteProductPayload, name: e.target.value})}/>
+        <Button onClick={() => mutationDeleteProduct.mutate()}>Delete Product</Button>
+        <div>
+        {mutationDeleteProduct.isPending ? (
+          'Adding todo...'
+        ) : (
+          <>
+            {mutationDeleteProduct.isError ? (
+              <div>An error occurred: {mutationDeleteProduct.error.message}</div>
+            ) : null}
+
+            {mutationDeleteProduct.isSuccess ? <div>Todo added!</div> : null}
           </>
         )}
       </div>
