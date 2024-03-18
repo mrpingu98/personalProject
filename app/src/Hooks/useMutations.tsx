72029bec224c851923bc7714b4d8f0297eb124c4
mrpingu98 <salh98manvir@gmail.com>
2024-03-18 21:26:55 +0000

    MS48 Snackbar state for useMutations

--- app/src/Hooks/useMutations.tsx
+++ app/src/Hooks/useMutations.tsx
@@ -1,33 +1,39 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
+import React from "react";
 
 
 export const useMutationPost = (url: string, payload: any, key?: string) => {
+    const [snackbar, setSnackbar] = React.useState<boolean>(false);
     const queryClient = useQueryClient()
     
     const mutation = useMutation({
             mutationFn: () => {return axios.post(url, payload)},
-            onSuccess: () => {queryClient.invalidateQueries({queryKey: [key], refetchType: 'all'})}
+            onSuccess: () => {queryClient.invalidateQueries({queryKey: [key], refetchType: 'all'}), setSnackbar(true)},
         })
-    return {mutation}
+    return {mutation, snackbar, setSnackbar}
 }
 
 export const useMutationPut = (url: string, payload: any, key?: string) => {
+    const [snackbar, setSnackbar] = React.useState<boolean>(false);
     const queryClient = useQueryClient()
+
     const mutation = useMutation({
         mutationFn: () => {return axios.put(url, payload)},
-        onSuccess: () => {queryClient.invalidateQueries({queryKey: [key], refetchType: 'all'})}
+        onSuccess: () => {queryClient.invalidateQueries({queryKey: [key], refetchType: 'all'}), setSnackbar(true)}
     })
-    return {mutation}
+    return {mutation, snackbar, setSnackbar}
 }
 
 export const useMutationDelete = (url: string, payload: any, key?: string) => {
+    const [snackbar, setSnackbar] = React.useState<boolean>(false);
     const queryClient = useQueryClient()
+
     const mutation = useMutation({
         mutationFn: () => {return axios.delete(url, {data: payload})},
-        onSuccess: () => {queryClient.invalidateQueries({ queryKey: [key], refetchType: 'all'})}
+        onSuccess: () => {queryClient.invalidateQueries({ queryKey: [key], refetchType: 'all'}), setSnackbar(true)}
     })
-    return {mutation}
+    return {mutation, snackbar, setSnackbar}
 }
 
 //By default invalidateQueries() only forces refetch for active queries, and for some reason the loader doesn't count as being "active", 
