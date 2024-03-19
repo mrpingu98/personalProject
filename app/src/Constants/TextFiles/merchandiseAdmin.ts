438929d879d57001aeefd2e502f607796f6a4a1c
mrpingu98 <salh98manvir@gmail.com>
2024-03-19 11:40:41 +0000

    MS48 updating text files

--- app/src/Constants/TextFiles/merchandiseAdmin.ts
+++ app/src/Constants/TextFiles/merchandiseAdmin.ts
@@ -6,5 +6,8 @@ export const merchandiseAdmin = {
     newName: 'New Name',
     addProduct: 'Add Product',
     editProduct: 'Edit Product',
-    deleteProduct: 'Delete Product'
+    deleteProduct: 'Delete Product',
+    productAdded: 'Product added',
+    productUpdated: 'Product Updated',
+    productDeleted: 'Product Deleted'
 }
