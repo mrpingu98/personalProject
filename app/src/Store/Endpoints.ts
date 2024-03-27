0513038f3d5c921e991992de914f8b1a1e540e0e
mrpingu98 <salh98manvir@gmail.com>
2024-03-27 15:54:36 +0000

    API Endpoint configuration

--- app/src/Store/Endpoints.ts
+++ app/src/Store/Endpoints.ts
@@ -1,3 +1,11 @@
+
+let apiUrl;
+if (process.env.NODE_ENV === 'development'){
+    apiUrl = process.env.REACT_APP_API_URL_DEV
+} else {
+    apiUrl = process.env.REACT_APP_API_URL_PROD
+}
+
 export const apiEndpoints = {
     spotifyUserTopTracks: `https://api.spotify.com/v1/me/top/tracks?limit=${10}`,
     spotifyAllTimeUserTopTracks: `https://api.spotify.com/v1/me/top/tracks?limit=${10}&time_range=long_term`,
@@ -6,5 +14,5 @@ export const apiEndpoints = {
     spotifyUserTopArtists: `https://api.spotify.com/v1/me/top/artists?limit=${10}`,
     spotifyAllTimeUserTopArtists: `https://api.spotify.com/v1/me/top/artists?limit=${10}&time_range=long_term`,
     spotifyUserPlaylists: `https://api.spotify.com/v1/me/playlists?limit=${10}`,
-    products: 'https://chimeraapi.azurewebsites.net/product/GetAllProducts',
+    products: `${apiUrl}/product/GetAllProducts`,
 }
