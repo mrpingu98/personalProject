0406120e3c1ae2d86a93fab70414829a9309fd2f
salhmanvir <Manvir.Salh@kpmg.co.uk>
2024-04-12 14:44:07 +0100

    text files

--- app/src/Constants/TextFiles/myProfile.ts
+++ app/src/Constants/TextFiles/myProfile.ts
@@ -3,6 +3,9 @@ export const myProfile = {
     spotifyDescription: 'To view the music sections of this website you need to give authorisation to use your personal Spotify data',
     haveAuthorised: 'You have authorised access to your Spotify - thank you',
     spotifyAuthorisation: 'Spotify Authorisation',
-    myProfile: 'My Profile'
-
+    myProfile: 'My Profile',
+    setTheme: 'Set a light/dark theme',
+    theme: 'Theme',
+    lightTheme: 'Light Theme',
+    darkTheme: 'Dark Theme'
 }
