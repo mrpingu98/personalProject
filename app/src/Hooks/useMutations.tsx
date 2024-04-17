9899458f04165c785b9ab41cc68005a9d0c877e1
mrpingu98 <salh98manvir@gmail.com>
2024-04-17 11:08:54 +0100

    MS65 SPotify useMutation token refresh

--- app/src/Hooks/useMutations.tsx
+++ app/src/Hooks/useMutations.tsx
@@ -1,6 +1,7 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
 import React from "react";
+import { spotifyPost } from "../Store/apiStore";
 
 
 export const useMutationPost = (url: string, payload: any, key?: string) => {
@@ -9,7 +10,7 @@ export const useMutationPost = (url: string, payload: any, key?: string) => {
     
     const mutation = useMutation({
             mutationFn: () => {return axios.post(url, payload)},
-            onSuccess: () => {queryClient.invalidateQueries({queryKey: [key], refetchType: 'all'}), setSnackbar(true)},
+            onSuccess: () => {queryClient.invalidateQueries({queryKey: [key], refetchType: 'all'}), setSnackbar(true)}
         })
     return {mutation, snackbar, setSnackbar}
 }
@@ -38,3 +39,25 @@ export const useMutationDelete = (url: string, payload: any, key?: string) => {
 
 //By default invalidateQueries() only forces refetch for active queries, and for some reason the loader doesn't count as being "active", 
 //so I had to add the prop refetchType: 'all' to invalidateQueries()
+
+
+export const useMutationSpotifyPost = (url: string, key?: string) => {
+    const queryClient = useQueryClient() 
+    const refreshToken = localStorage.getItem("refresh_token");
+    const payload = {
+      grant_type: "refresh_token",
+      refresh_token: refreshToken == null ? "" : refreshToken,
+      client_id: "44deba64e2b04230a4e7c818ca419918",
+    }
+    
+    const mutation = useMutation({
+            mutationFn: () => {
+                return spotifyPost(url, payload)},
+            onSuccess: (data) => {
+                queryClient.invalidateQueries({queryKey: [key], refetchType: 'all'}), 
+                data?.access_token && localStorage.setItem("access_token", data.access_token),
+                data?.refresh_token && localStorage.setItem("refresh_token", data.refresh_token)
+            },
+        })
+    return {mutation}
+}
