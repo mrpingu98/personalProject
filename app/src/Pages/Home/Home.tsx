c8cdd774c6ddcbb06c08aed5ada75cd7efb4bf8a
mrpingu98 <salh98manvir@gmail.com>
2024-04-18 10:19:01 +0100

    MS65 refresh token home page

--- app/src/Pages/Home/Home.tsx
+++ app/src/Pages/Home/Home.tsx
@@ -2,16 +2,26 @@ import React from "react";
 import { Box, Typography } from "@mui/material";
 import { useTranslation } from "react-i18next";
 import { styled } from '@mui/system';
+import { useMutationSpotifyPost } from "../../Hooks/useMutations";
+import { apiEndpoints } from "../../Store/Endpoints";
 
 const Home: React.FC = () => {
   const { t } = useTranslation("home");
-  
+  const { mutation } = useMutationSpotifyPost(apiEndpoints.spotifyTokenRequest)
+
+  React.useEffect(() => {
+    const checkToken = localStorage.getItem("access_token") ? true : false
+    if (checkToken) {
+      mutation.mutate()
+    }
+  }, [])
+
   return (
     <MainContainer>
-        <Typography variant="h1" data-test-id={"home-title"}>{t("welcome")}</Typography>
-        <Box marginTop={"2%"}>
-          <Typography variant="body1" data-test-id={"home-description"}>{t("welcomeMessage")}</Typography>
-        </Box>
+      <Typography variant="h1" data-test-id={"home-title"}>{t("welcome")}</Typography>
+      <Box marginTop={"2%"}>
+        <Typography variant="body1" data-test-id={"home-description"}>{t("welcomeMessage")}</Typography>
+      </Box>
     </MainContainer>
   );
 };
