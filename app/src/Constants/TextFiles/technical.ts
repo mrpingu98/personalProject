4344c23ba1f9e69a5b7cf5a2a6738ac8e7d7d345
mrpingu98 <salh98manvir@gmail.com>
2024-05-11 10:44:27 +0100

    adding content to technical page

---
+++ app/src/Constants/TextFiles/technical.ts
@@ -0,0 +1,14 @@
+export const technical = {
+    mainTitle: 'Aim of project',
+    mainDescriptionOne: 'My aim is to be a full-stack developer. When I started this project I had only a basic understanding of the full layout of an app (front-end, back-end, deployment, etc). So I set myself the challenge of trying to develop and deploy one.',
+    mainDescriptionTwo: 'Where I had gaps in my knowledge, I went away and did self-learning, then came back and applied what I had learnt. I’ve found making this actually quite fun - understanding the configuration, how to deploy, etc. I love problem-solving and getting technical, which this project has allowed me to do.',
+    mainDescriptionThree: 'Below is a summary of the technical skills I’ve applied/learnt',
+    setupTitle: 'General Setup',
+    setupDescriptionOne: 'JIRA - made a personal board to create tickets and track progress. Tickets labelled as ‘MS<number>’',
+    setupDescriptionTwo: 'Github - Simple branching strategy (task branch off main, pushed back into main). Branch naming based on JIRA ticket Id ‘MS<number>’',
+    frontEndRepo: 'Front-end repo (React / Typescript): ',
+    frontEndRepoUrl: 'https://github.com/mrpingu98/personalProject',
+    backEndRepo: 'Back-end repo (.NET 8 / C#): ',
+    backEndRepoUrl: 'https://github.com/mrpingu98/personalProjectAPI'
+
+}
--- app/src/Pages/Technical/Technical.tsx
+++ app/src/Pages/Technical/Technical.tsx
@@ -1,10 +1,57 @@
 import React from 'react'
+import { styled } from '@mui/system';
+import { Typography, Box } from '@mui/material';
+import { useTranslation } from 'react-i18next';
 
 const Technical: React.FC = () => {
+    const {t} = useTranslation("technical")
 
     return(
-        <div></div>
+        <>       
+        <Title>
+            <Typography variant="h3">{t('mainTitle')}</Typography>
+        </Title>
+        <Description>
+            <Typography variant='body1' marginTop={4}>{t('mainDescriptionOne')}</Typography>
+            <Typography variant='body1' marginTop={4}>{t('mainDescriptionTwo')}</Typography>
+            <Typography variant='body1' marginTop={4}>{t('mainDescriptionThree')}</Typography>
+        </Description>
+        <Title>
+            <Typography variant="h3" marginTop={8}>{t('setupTitle')}</Typography>
+        </Title>
+        <Description>
+            <ul>
+                <li>
+                    <Typography variant="body1">{t('setupDescriptionOne')}</Typography>
+                </li>
+                <li>
+                    <Typography variant="body1" marginTop={2}>{t('setupDescriptionTwo')}</Typography>
+                </li>
+                <li>
+                    <Box flexDirection="row" display = "flex">
+                    <Typography variant="body1" marginTop={2}>{t('frontEndRepo')}<a href={t('frontEndRepoUrl')} target='_blank'>{t('frontEndRepoUrl')}</a></Typography>
+                    </Box>
+                </li>
+                <li>
+                    <Box flexDirection="row" display = "flex">
+                    <Typography variant="body1" marginTop={2}>{t('backEndRepo')}<a href={t('backEndRepoUrl')} target='_blank'>{t('backEndRepoUrl')}</a></Typography>
+                    </Box>
+                </li>
+            </ul>
+        </Description>
+
+        </>
     )
 }
+
+const Description = styled('div')({
+    alignItems: "flex-start"
+  });
+  
+  const Title = styled('div')({
+    alignItems: "center",
+      display: "flex",
+      flexDirection: "column",
+  });
 
 export {Technical}
