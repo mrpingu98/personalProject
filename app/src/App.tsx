7fe21e42a989b0a9964661373de41971a90d16c0
mrpingu98 <123986231+mrpingu98@users.noreply.github.com>
2024-07-29 15:58:20 +0100

    Ms 84 update routing (#45)
    * MS84 Updating routing
    
    * MS84 removing old routing

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,37 +1,39 @@
-import { HomeRoute } from "./Constants/Routes/HomeRoutes";
-import { BrowserRouter } from "react-router-dom";
-import { MixesRoute } from "./Constants/Routes/MixesRoutes";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./Components/Layout/Layout";
-import { MusicRoute } from "./Constants/Routes/MusicRoute";
 import './Configuration/configurei18n';
 import './index.css'
 import { store } from "./Store/configureStore";
 import { Provider } from 'react-redux'
-import { PersonalisedSpotifyRoute } from "./Constants/Routes/PersonalisedSpotify";
-import { MyProfileRoute } from "./Constants/Routes/MyProfileRoute";
-import { ErrorRoute } from "./Constants/Routes/ErrorRoute";
-import { MerchandiseRoute } from "./Constants/Routes/MerchandiseRoute";
-import { MerchandiseAdminRoute } from "./Constants/Routes/MerchandiseAdminRoute";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { TechnicalRoute } from "./Constants/Routes/TechnicalRoute";
+import { Home } from "./Pages/Home/Home";
+import { routes } from "./Constants/routes";
+import { Merchandise } from "./Pages/Merchandise/Merchandise";
+import { MerchandiseAdmin } from "./Pages/MerchandiseAdmin/MerchandiseAdmin";
+import { Mixes } from "./Pages/Mixes/Mixes";
+import { Music } from "./Pages/Music/Music";
+import { MyProfile } from "./Pages/MyProfile/MyProfile";
+import { PersonalisedSpotify } from "./Pages/PersonalisedSpotify/PersonalisedSpotify";
+import { Error } from "./Pages/Error/Error";
+import { Technical } from "./Pages/Technical/Technical";
+
 
 const queryClient = new QueryClient();
 
 function AppRoutes() {
-  return (
-    <>
-      <HomeRoute />
-      <MixesRoute />
-      <MusicRoute />
-      <PersonalisedSpotifyRoute />
-      <MyProfileRoute />
-      <ErrorRoute />
-      <MerchandiseRoute />
-      <MerchandiseAdminRoute />
-      <TechnicalRoute />
-    </>
-  );
+  return(
+    <Routes>
+            <Route path={routes.home} element={<Home />} />
+            <Route path={routes.merchandise} element={<Merchandise />} />
+            <Route path={routes.merchandiseAdmin} element={<MerchandiseAdmin />} />
+            <Route path={routes.mixes} element={<Mixes />} />
+            <Route path={routes.music}element={<Music />} />
+            <Route path={routes.myProfile} element={<MyProfile />} />
+            <Route path={routes.personalisedSpotify} element={<PersonalisedSpotify />} />
+            <Route path={routes.error} element={<Error />} />
+            <Route path={routes.technical} element={<Technical />} />
+    </Routes>
+  )
 }
 
 function App() {
@@ -39,12 +41,10 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <Provider store={store}>
-          {/* <DarkThemeContextProvider> */}
           <Layout>
               <AppRoutes />
               <ReactQueryDevtools initialIsOpen={false} />
           </Layout>
-          {/* </DarkThemeContextProvider> */}
         </Provider>
       </BrowserRouter>
     </QueryClientProvider>
